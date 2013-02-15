994824a09a7bedf10f3bee0106ce152e85497773
jbeerdev <jbeerbros@gmail.com>
2013-02-15 11:48:34 +0100

    Added getters and setters for the Tweet class

--- src/jbeer/dev/twittermonitor/data/domain/Tweet.java
+++ src/jbeer/dev/twittermonitor/data/domain/Tweet.java
@@ -2,9 +2,35 @@ package jbeer.dev.twittermonitor.data.domain;
 
 public class Tweet {
 	
-	public String userName;
-	public String avatarURl;
-	public String time;
-	public String tweet;
+	private String userName;
+	private String avatarURl;
+	private String time;
+	private String tweet;
+	
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getAvatarURl() {
+		return avatarURl;
+	}
+	public void setAvatarURl(String avatarURl) {
+		this.avatarURl = avatarURl;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getTweet() {
+		return tweet;
+	}
+	public void setTweet(String tweet) {
+		this.tweet = tweet;
+	}
 
 }
